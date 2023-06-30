91d6e91d22d8d2529257deea51af5a805184be57
Zi Gang <tanzigang93@gmail.com>
2023-07-01 01:04:25 +0800

    test: add test case to reproduce problem

---
+++ packages/logger/src/node/methods.ts
@@ -0,0 +1,40 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-console */
+import { captureException, captureMessage } from '@sentry/node';
+
+/** Send exception to the error logger from node. */
+export const logError = (message: Error | string, withConsole?: boolean) => {
+  captureException(message);
+  if (withConsole) {
+    console.error(message);
+  }
+};
+
+/** Send warning to the error logger from node. */
+export const logWarning = (warning: string, withConsole?: boolean) => {
+  captureMessage(warning, 'warning');
+  if (withConsole) {
+    console.warn(warning);
+  }
+};
+
+/** Send an info log to the error logger from node. */
+export const logInfo = (info: string, withConsole?: boolean) => {
+  captureMessage(info, 'info');
+  if (withConsole) {
+    console.info(info);
+  }
+};
+
+/** Send a debug log to the error logger from node. */
+export const logDebug = (debugMessage: string, withConsole?: boolean) => {
+  captureMessage(debugMessage, 'debug');
+  if (withConsole) {
+    console.warn(debugMessage);
+  }
+};
+
+/** Log a message to the console. This method will not send an error report. */
+export const consoleLog = (message: string) => {
+  console.log(message);
+};
